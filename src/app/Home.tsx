075f755a084d2d43c307322fcb01a10d4c4f7215
Josh Elgar <joshelgar@gmail.com>
2024-02-08 22:09:54 +0000

    Favicons to public

--- src/app/Home.tsx
+++ src/app/Home.tsx
@@ -1,9 +1,15 @@
+import Head from "next/head";
 import LaunchesGrid from "./LaunchesGrid";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <LaunchesGrid />
-    </main>
+    <>
+      <Head>
+        <link rel="icon" href="/hnlaunches/src/app/favicon.ico" />
+      </Head>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <LaunchesGrid />
+      </main>
+    </>
   );
 }
